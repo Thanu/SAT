4efa62f28bd278338c3bf8f2588d03f4b3ed69bc
Thanuja Uruthira <uthanuja@yahoo.com>
2014-11-11 18:13:32 +0530

    Delete PropertyFile.java

--- SAT/src/main/java/com/project/traceability/common/PropertyFile.java
+++
@@ -1,9 +0,0 @@
-package com.project.traceability.common;
-
-public class PropertyFile {
-	
-	public static final String requirementXMLPath = "E:\\Research Project\\projectclarification\\RequirementArtefactFile.xml";
-	public static final String umlXMLPath = "E:\\Research Project\\projectclarification\\UMLArtefactFile.xml";
-	public static final String sourceXMLPath = "E:\\Research Project\\projectclarification\\SourceCodeArtefactFile.xml";
-	public static final String relationshipXMLPath = "E:\\Research Project\\projectclarification\\createFile.xml";
-}

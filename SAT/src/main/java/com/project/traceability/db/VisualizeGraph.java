a8771295814c31db5c4936e10e2d61a42f20cfee
Thanuja Uruthira <uthanuja@yahoo.com>
2014-11-11 16:25:34 +0530

    Adding new files.

---
+++ SAT/src/main/java/com/project/traceability/db/VisualizeGraph.java
@@ -0,0 +1,225 @@
+package com.project.traceability.db;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+import javax.swing.JFrame;
+
+import org.gephi.data.attributes.api.AttributeColumn;
+import org.gephi.data.attributes.api.AttributeController;
+import org.gephi.data.attributes.api.AttributeModel;
+import org.gephi.graph.api.DirectedGraph;
+import org.gephi.graph.api.GraphController;
+import org.gephi.graph.api.GraphModel;
+import org.gephi.io.importer.api.Container;
+import org.gephi.io.importer.api.ImportController;
+import org.gephi.io.processor.plugin.DefaultProcessor;
+import org.gephi.layout.plugin.AutoLayout;
+import org.gephi.layout.plugin.force.StepDisplacement;
+import org.gephi.layout.plugin.force.yifanHu.YifanHuLayout;
+import org.gephi.layout.plugin.forceAtlas2.ForceAtlas2;
+import org.gephi.layout.plugin.labelAdjust.LabelAdjust;
+import org.gephi.partition.api.EdgePartition;
+import org.gephi.partition.api.NodePartition;
+import org.gephi.partition.api.PartitionController;
+import org.gephi.partition.plugin.EdgeColorTransformer;
+import org.gephi.partition.plugin.NodeColorTransformer;
+import org.gephi.preview.api.ManagedRenderer;
+import org.gephi.preview.api.PreviewController;
+import org.gephi.preview.api.PreviewModel;
+import org.gephi.preview.api.PreviewProperty;
+import org.gephi.preview.api.ProcessingTarget;
+import org.gephi.preview.api.RenderTarget;
+import org.gephi.preview.types.DependantColor;
+import org.gephi.preview.types.DependantOriginalColor;
+import org.gephi.preview.types.EdgeColor;
+import org.gephi.project.api.ProjectController;
+import org.gephi.project.api.Workspace;
+import org.gephi.ranking.api.Ranking;
+import org.gephi.ranking.api.RankingController;
+import org.gephi.ranking.api.Transformer;
+import org.gephi.ranking.plugin.transformer.AbstractSizeTransformer;
+import org.gephi.statistics.plugin.GraphDistance;
+import org.openide.util.Exceptions;
+import org.openide.util.Lookup;
+
+import processing.core.PApplet;
+
+
+public class VisualizeGraph {
+	// static private MyGlassPane myGlassPane;
+
+	public static void main(String[] args) {
+		VisualizeGraph preview = new VisualizeGraph();
+		preview.script();
+		
+	}
+
+	public void script() {
+		// Init a project - and therefore a workspace
+		ProjectController pc = Lookup.getDefault().lookup(
+				ProjectController.class);
+		pc.newProject();
+		Workspace workspace = pc.getCurrentWorkspace();
+
+		// Import file
+		ImportController importController = Lookup.getDefault().lookup(
+				ImportController.class);
+		Container container;
+		try {
+			File file = new File("C:\\Users\\Thanu\\Documents\\proj-3.gexf");
+			container = importController.importFile(file);
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			return;
+		}
+
+		// Append imported data to GraphAPI
+		importController.process(container, new DefaultProcessor(), workspace);
+
+		// Preview configuration
+		PreviewController previewController = Lookup.getDefault().lookup(
+				PreviewController.class);
+		PreviewModel previewModel = previewController.getModel();
+		
+		previewModel.getProperties().putValue(PreviewProperty.SHOW_NODE_LABELS,Boolean.TRUE);
+		previewModel.getProperties().putValue(
+				PreviewProperty.NODE_LABEL_PROPORTIONAL_SIZE, Boolean.TRUE);
+		previewModel.getProperties().putValue(PreviewProperty.NODE_LABEL_COLOR,
+				new DependantOriginalColor(Color.BLACK));
+		previewModel.getProperties().putValue(
+				PreviewProperty.NODE_BORDER_WIDTH, 30.0f);
+		Font f = previewModel.getProperties().getFontValue(PreviewProperty.NODE_LABEL_FONT);		
+		previewModel.getProperties().putValue(PreviewProperty.NODE_LABEL_FONT, f.deriveFont(Font.BOLD,f.getSize()-9));
+		previewModel.getProperties().putValue(
+				PreviewProperty.NODE_BORDER_COLOR,
+				new DependantColor(DependantColor.Mode.PARENT));
+		previewModel.getProperties()
+				.putValue(PreviewProperty.NODE_OPACITY, 100);
+		
+		previewModel.getProperties().putValue(PreviewProperty.EDGE_CURVED,
+				Boolean.FALSE);
+		previewModel.getProperties()
+		.putValue(PreviewProperty.EDGE_COLOR,new EdgeColor(EdgeColor.Mode.ORIGINAL));
+		previewModel.getProperties()
+				.putValue(PreviewProperty.EDGE_OPACITY, 100);
+		previewModel.getProperties().putValue(PreviewProperty.EDGE_THICKNESS,
+				10.0);
+		previewModel.getProperties().putValue(PreviewProperty.EDGE_RADIUS, 1f);//distance between node and edge arrow
+		previewModel.getProperties().putValue(PreviewProperty.SHOW_EDGE_LABELS,
+				Boolean.TRUE);
+		previewModel.getProperties().putValue(PreviewProperty.EDGE_LABEL_COLOR,
+				new DependantOriginalColor(Color.BLACK));
+		f = previewModel.getProperties().getFontValue(PreviewProperty.EDGE_LABEL_FONT);
+		previewModel.getProperties().putValue(PreviewProperty.EDGE_LABEL_FONT, f.deriveFont(Font.BOLD,f.getSize()+50));
+		previewModel.getProperties().putValue(PreviewProperty.BACKGROUND_COLOR,
+				Color.GRAY);
+		previewController.refreshPreview();
+
+		GraphModel graphModel = Lookup.getDefault()
+				.lookup(GraphController.class).getModel();
+
+		AttributeModel attributeModel = Lookup.getDefault()
+				.lookup(AttributeController.class).getModel();
+//		FilterController filterController = Lookup.getDefault().lookup(
+//				FilterController.class);
+		RankingController rankingController = Lookup.getDefault().lookup(
+				RankingController.class);
+
+		// See if graph is well imported
+		DirectedGraph graph = graphModel.getDirectedGraph();
+		System.out.println(graph.getEdgeCount());
+
+
+//		// Get Centrality
+//		GraphDistance distance = new GraphDistance();
+//		distance.setDirected(false);
+//		distance.execute(graphModel, attributeModel);
+
+		// Rank size by eccentricity
+		Ranking eccentricityRanking = rankingController.getModel().getRanking(
+				Ranking.NODE_ELEMENT, GraphDistance.ECCENTRICITY);
+		AbstractSizeTransformer sizeTransformer = (AbstractSizeTransformer) rankingController
+				.getModel().getTransformer(Ranking.NODE_ELEMENT,
+						Transformer.RENDERABLE_SIZE);
+		sizeTransformer.setMinSize(200.0f);
+		sizeTransformer.setMaxSize(180.0f);
+		rankingController.transform(eccentricityRanking, sizeTransformer);
+
+		// Partition with 'type' column, which is in the data
+		PartitionController partitionController = Lookup.getDefault().lookup(
+				PartitionController.class);
+		AttributeColumn []att = attributeModel.getNodeTable().getColumns();
+		for(AttributeColumn a:att){
+		System.out.println(partitionController.buildPartition(a,graph));
+		}
+		NodePartition node_partition = (NodePartition) partitionController.buildPartition(
+				attributeModel.getNodeTable().getColumn("Type"), graph);
+		NodeColorTransformer nodeColorTransformer = new NodeColorTransformer();
+		nodeColorTransformer.randomizeColors(node_partition);
+		partitionController.transform(node_partition, nodeColorTransformer);
+
+		// Partition with 'Neo4j Relationship Type' column, which is in the data
+		EdgePartition edge_partition = (EdgePartition) partitionController.buildPartition(
+				attributeModel.getEdgeTable().getColumn(
+						"Neo4j Relationship Type"), graph);
+		//System.out.println("#Relation partitions found: "  + edge_partition.getPartsCount());
+    	
+		EdgeColorTransformer edgeColorTransformer = new EdgeColorTransformer();
+		edgeColorTransformer.randomizeColors(edge_partition);
+		partitionController.transform(edge_partition, edgeColorTransformer);		
+		
+		AutoLayout autoLayout = new AutoLayout(1, TimeUnit.SECONDS);
+		autoLayout.setGraphModel(graphModel);
+		YifanHuLayout firstLayout = new YifanHuLayout(null, new StepDisplacement(1f));
+		ForceAtlas2 secondLayout = new ForceAtlas2(null);
+		LabelAdjust thirdLayout = new LabelAdjust(null);
+		AutoLayout.DynamicProperty adjustBySizeProperty = AutoLayout.createDynamicProperty("forceAtlas2.adjustSizes.name", Boolean.TRUE, 0.2f);//True after 10% of layout time
+		AutoLayout.DynamicProperty linLogModeProperty = AutoLayout.createDynamicProperty("forceAtlas2.linLogMode.name",Boolean.FALSE , 0f);//500 for the complete period
+		AutoLayout.DynamicProperty gravityProperty = AutoLayout.createDynamicProperty("forceAtlas2.gravity.name",100d,0f);
+		AutoLayout.DynamicProperty scallingRatioProperty = AutoLayout.createDynamicProperty("forceAtlas2.scalingRatio.name",2000d,0f);
+		autoLayout.addLayout(firstLayout, 0.0f);
+		autoLayout.addLayout(secondLayout, 0.5f, new AutoLayout.DynamicProperty[]{adjustBySizeProperty,linLogModeProperty, gravityProperty,scallingRatioProperty});
+		autoLayout.addLayout(thirdLayout,0.5f);
+		autoLayout.execute();
+		
+		// New Processing target, get the PApplet
+		ProcessingTarget target = (ProcessingTarget) previewController
+				.getRenderTarget(RenderTarget.PROCESSING_TARGET);
+		target.zoomPlus();
+		target.zoomPlus();
+		target.zoomPlus();
+		target.zoomPlus();
+		
+		PApplet applet = target.getApplet();
+		applet.init();
+	
+				
+		try {
+            Thread.sleep(100);
+        } catch (InterruptedException ex) {
+            Exceptions.printStackTrace(ex);
+        }
+
+		// Refresh the preview and reset the zoom
+		previewController.render(target);
+		target.refresh();
+		target.resetZoom();
+		
+
+		// Add the applet to a JFrame and display
+		JFrame frame = new JFrame("Test Preview");
+		frame.setLayout(new BorderLayout());
+
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.add(applet, BorderLayout.CENTER);
+		
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
--- sat/src/main/java/com/project/traceability/db/GraphDB.java
+++ sat/src/main/java/com/project/traceability/db/GraphDB.java
@@ -159,7 +159,7 @@ public class GraphDB{
 					n.setProperty("ID", artefactElement.getArtefactElementId());
 					n.setProperty("Name", artefactElement.getName());
 					n.setProperty("Type", artefactElement.getType());
-					addNode(artefactElement.getArtefactElementId(),50,node_count*100);
+					//addNode(artefactElement.getArtefactElementId(),50,node_count*100);
 					artefacts.add(n, "ID", n.getProperty("ID"));
 					List<ArtefactSubElement> artefactsSubElements = artefactElement
 							.getArtefactSubElements();
@@ -175,7 +175,7 @@ public class GraphDB{
 						//node_count++;
 						int x=node_count*50;
 						int y = node_count*50;
-						addNode(temp.getSubElementId(),y,x+(100*(i+1)));
+						//addNode(temp.getSubElementId(),y,x+(100*(i+1)));
 						if (null != temp.getVisibility()) {
 							m.setProperty("Visibility", temp.getVisibility());
 						}
@@ -213,7 +213,7 @@ public class GraphDB{
 						}
 						relationship = n.createRelationshipTo(m,
 								RelTypes.SUB_ELEMENT);
-						addEdge(node_count-1,nodes.size()-1);
+						//addEdge(node_count-1,nodes.size()-1);
 						relationship.setProperty("message",
 								RelTypes.SUB_ELEMENT.getValue());
 					}
@@ -320,125 +320,41 @@ public class GraphDB{
 		}
 	}
 
-	public GraphDB() {
-//		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		setBounds(100, 100, 794, 653);
-//		contentPane = new JPanel();
-//		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-//		contentPane.setLayout(null);
-//		setContentPane(contentPane);
-//		
-//		nodes = new ArrayList<GraphNode>();
-//		edges = new ArrayList<GraphEdge>();
-//		width = 60;
-//		height = 60;
-//	      
-
-//		final JTextArea queryTextArea = new JTextArea();
-//		queryTextArea.setBounds(25, 40, 702, 109);
-//		queryTextArea.setBorder(new EmptyBorder(5, 5, 5, 5));
-//		queryTextArea.setText("start n=node(*) return n, n.name");
-//		contentPane.add(queryTextArea);
-//
-//		JLabel cypherQueryLabel = new JLabel("Cypher Query");
-//		cypherQueryLabel.setBounds(25, 12, 105, 23);
-//		contentPane.add(cypherQueryLabel);
-//
-//		final JTextArea resultTextArea = new JTextArea();
-//		resultTextArea.setBounds(25, 181, 702, 341);
-//		resultTextArea.setEditable(false);
-//		resultTextArea.setBorder(new EmptyBorder(5, 5, 5, 5));
-//		contentPane.add(resultTextArea);
-//
-//		JLabel resultLabel = new JLabel("Result:");
-//		resultLabel.setBounds(25, 323, 55, 23);
-//
-//		contentPane.add(resultLabel);
-//
-//		JButton btnNewButton_2 = new JButton("Execute Query");
-//		btnNewButton_2.setBounds(274, 540, 176, 41);
-//		btnNewButton_2.setVisible(true);
-//		contentPane.add(btnNewButton_2);
-//
-//		btnNewButton_2.addActionListener(new ActionListener() {
-//
-//			public void actionPerformed(ActionEvent e) {
-//
-//				ExecutionEngine engine = new ExecutionEngine(graphDb);
-//				ExecutionResult result = engine.execute(queryTextArea.getText());
-//				String rows = "";
-//				for (Map<String, Object> row : result) {
-//					for (Entry<String, Object> column : row.entrySet()) {
-//						rows += column.getKey() + ": " + column.getValue()
-//								+ "; ";
-//					}
-//					rows += "\n";
-//				}
-//				resultTextArea.setText(rows);
-//			}
-//		});
-	}
 	int width;
     int height;
     int node_count=0;
-    ArrayList<GraphNode> nodes;
-    ArrayList<GraphEdge> edges;
-
-	class GraphNode {
-		int x, y;
-		String name;
-
-		public GraphNode(String myName, int myX, int myY) {
-			x = myX;
-			y = myY;
-			name = myName;
-		}
-	}
-
-	class GraphEdge {
-		int i, j;
-		public GraphEdge(int ii, int jj) {
-			i = ii;
-			j = jj;
-		}
-	}
-
-	public void addNode(String name, int x, int y) {
-		// add a node at pixel (x,y)
-		nodes.add(new GraphNode(name, x, y));
-		//this.repaint();
-	}
-
-	public void addEdge(int i, int j) {
-		// add an edge between nodes i and j
-		edges.add(new GraphEdge(i, j));
-		//this.repaint();
-	}
-
-//	public void paint(Graphics g) { // draw the nodes and edges
-//		FontMetrics f = g.getFontMetrics();
-//		int nodeHeight = Math.max(height, f.getHeight());
+//    ArrayList<GraphNode> nodes;
+//    ArrayList<GraphEdge> edges;
 //
-//		g.setColor(Color.black);
-//		for (GraphEdge e : edges) {
-//			g.drawLine(nodes.get(e.i).x, nodes.get(e.i).y, nodes.get(e.j).x,
-//					nodes.get(e.j).y);
-//			g.setColor(Color.YELLOW);
+//	class GraphNode {
+//		int x, y;
+//		String name;
+//
+//		public GraphNode(String myName, int myX, int myY) {
+//			x = myX;
+//			y = myY;
+//			name = myName;
 //		}
+//	}
 //
-//		for (GraphNode n : nodes) {
-//			int nodeWidth = Math.max(width, f.stringWidth(n.name) + width / 2);
-//			g.setColor(new Color(new Random().nextInt()));
-//			g.fillOval(n.x - nodeWidth / 2, n.y - nodeHeight / 2, nodeWidth,
-//					nodeHeight);
-//			g.setColor(Color.black);
-//			g.drawOval(n.x - nodeWidth / 2, n.y - nodeHeight / 2, nodeWidth,
-//					nodeHeight);
-//
-//			g.drawString(n.name, n.x - f.stringWidth(n.name) / 2,
-//					n.y + f.getHeight() / 2);
-//			
+//	class GraphEdge {
+//		int i, j;
+//		public GraphEdge(int ii, int jj) {
+//			i = ii;
+//			j = jj;
 //		}
+//	}
+//
+//	public void addNode(String name, int x, int y) {
+//		// add a node at pixel (x,y)
+//		nodes.add(new GraphNode(name, x, y));
+//		//this.repaint();
+//	}
+//
+//	public void addEdge(int i, int j) {
+//		// add an edge between nodes i and j
+//		edges.add(new GraphEdge(i, j));
+//		//this.repaint();
 //	}
 
 }

7ac5fcb815a5f9e6b407a6094ddabbc486c7fac8
Kamalabalan <kamalan89@gmail.com>
2014-11-13 16:31:16 +0530

    PropertyFile updated

--- sat/src/main/java/com/project/traceability/common/PropertyFile.java
+++ sat/src/main/java/com/project/traceability/common/PropertyFile.java
@@ -13,5 +13,8 @@ public class PropertyFile {
 	public static final String umlXMLPath = "E:\\Research Project\\projectclarification\\UMLArtefactFile.xml";
 	public static final String sourceXMLPath = "E:\\Research Project\\projectclarification\\SourceCodeArtefactFile.xml";
 	public static final String relationshipXMLPath = "E:\\Research Project\\projectclarification\\createFile.xml";
+	public static final String graphDbPath = "E:\\Neo4j\\atomdb.graphdb";
+	public static final String graphWriterPath = "C:\\Users\\K.Kamalan\\Documents\\atom-new.gexf";
+	public static final String generatedGxfFilePath = "C:\\Users\\K.Kamalan\\Documents\\proj-3.gexf";
 }
 

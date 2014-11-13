047603c78488b9bb53306b3ffc569e13811e35a3
Kamalabalan <kamalan89@gmail.com>
2014-11-13 16:09:43 +0530

    PropertyFile added

---
+++ sat/src/main/java/com/project/traceability/common/PropertyFile.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.project.traceability.common;
+
+/**13 Nov 2014
+ * @author K.Kamalan
+ *
+ */
+public class PropertyFile {
+	
+	public static final String requirementXMLPath = "E:\\Research Project\\projectclarification\\RequirementArtefactFile.xml";
+	public static final String umlXMLPath = "E:\\Research Project\\projectclarification\\UMLArtefactFile.xml";
+	public static final String sourceXMLPath = "E:\\Research Project\\projectclarification\\SourceCodeArtefactFile.xml";
+	public static final String relationshipXMLPath = "E:\\Research Project\\projectclarification\\createFile.xml";
+}
+

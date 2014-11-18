5530bfa6a495fde0110fcb7feab3217f9a24e76e
Gitanjali <ganjali31@gmail.com>
2014-11-18 14:06:26 +0530

    compare class added

--- sat/src/main/java/com/project/traceability/GUI/CompareWindow.java
+++ sat/src/main/java/com/project/traceability/GUI/CompareWindow.java
@@ -1,6 +1,8 @@
 package com.project.traceability.GUI;
 
 import java.awt.Dimension;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Point;
@@ -13,10 +15,15 @@ import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.TabFolder;
 import org.eclipse.swt.widgets.TabItem;
 
+import com.project.traceability.manager.UMLSourceClassManager;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
+
 public class CompareWindow {
 
 	protected Shell shell;public static Dimension screen = java.awt.Toolkit.getDefaultToolkit()
 			.getScreenSize();
+	public static Table table;
 
 	/**
 	 * Launch the application.
@@ -25,7 +32,7 @@ public class CompareWindow {
 	public static void main(String[] args) {
 		try {
 			CompareWindow window = new CompareWindow();
-			window.open();
+			window.open("", new ArrayList<String>());
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -34,9 +41,10 @@ public class CompareWindow {
 	/**
 	 * Open the window.
 	 */
-	public void open() {
+	public void open(String project, ArrayList<String> selectedFiles) {
 		Display display = Display.getDefault();
-		createContents();
+		createContents(selectedFiles);
+		compareFiles(project, selectedFiles);
 		shell.open();
 		shell.layout();
 		center(shell);
@@ -46,11 +54,25 @@ public class CompareWindow {
 			}
 		}
 	}
+	
+	private void compareFiles(String project, ArrayList<String> selectedFiles) {
+		String filePath = "D:\\SATWork\\" + project + "\\";
+		System.out.println(selectedFiles.size());
+		if (selectedFiles.get(0).contains("UML") && selectedFiles.get(1).contains("Source")
+				|| selectedFiles.get(0).contains("Source")
+				&& selectedFiles.get(1).contains("UML")) {
+			List<String> relationNodes = UMLSourceClassManager
+					.compareClassNames(filePath);
+		} else if (selectedFiles.get(0).contains("Requirement")) {
+
+		}
+
+	}
 
 	/**
 	 * Create contents of the window.
 	 */
-	protected void createContents() {
+	protected void createContents(ArrayList<String> files) {
 		shell = new Shell();
 		shell.setSize(450, 300);
 		shell.setText("SWT Application");
@@ -67,7 +89,7 @@ public class CompareWindow {
 		tabFolder.setBounds(0, 0,(int) (screen.width * 0.5), (int) (screen.height * 0.5));
 		
 		TabItem tabItem = new TabItem(tabFolder, SWT.NONE);
-		tabItem.setText("New Item 1");
+		tabItem.setText(files.get(0));
 		
 		Composite composite = new Composite(tabFolder, SWT.NONE);
 		tabItem.setControl(composite);
@@ -77,7 +99,7 @@ public class CompareWindow {
 				(int) (screen.width * 0.5), (int) (screen.height * 0.5));
 		
 		TabItem tabItem_1 = new TabItem(tabFolder_1, SWT.NONE);
-		tabItem_1.setText("New Item 2");
+		tabItem_1.setText(files.get(1));
 		
 		Composite composite_1 = new Composite(tabFolder_1, SWT.NONE);
 		tabItem_1.setControl(composite_1);
@@ -86,10 +108,27 @@ public class CompareWindow {
 		tabFolder_2.setBounds(0, (int) (screen.height * 0.5),	screen.width, (int) (screen.height * 0.5));
 		
 		TabItem tabItem_2 = new TabItem(tabFolder_2, SWT.NONE);
-		tabItem_2.setText("New Item Merge");
+		tabItem_2.setText("Compared Results");
 		
 		Composite composite_2 = new Composite(tabFolder_2, SWT.NONE);
 		tabItem_2.setControl(composite_2);
+		
+		table = new Table(composite_2, SWT.BORDER | SWT.FULL_SELECTION);
+		table.setBounds(40, 31, 302, 224);
+		table.setHeaderVisible(true);
+		table.setLinesVisible(true);
+		
+		TableColumn tblclmnNewColumn = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn.setWidth(100);
+		tblclmnNewColumn.setText("Classes");
+		
+		TableColumn tblclmnNewColumn_1 = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn_1.setWidth(100);
+		tblclmnNewColumn_1.setText("Attributes");
+		
+		TableColumn tblclmnNewColumn_2 = new TableColumn(table, SWT.NONE);
+		tblclmnNewColumn_2.setWidth(100);
+		tblclmnNewColumn_2.setText("Methods");
 
 	}
 	

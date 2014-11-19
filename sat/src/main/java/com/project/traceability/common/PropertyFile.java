2873e3b90a96d7c00988ca200275d11c5bc3689a
Gitanjali <ganjali31@gmail.com>
2014-11-19 18:43:04 +0530

    group layout jar added

--- sat/src/main/java/com/project/traceability/common/PropertyFile.java
+++ sat/src/main/java/com/project/traceability/common/PropertyFile.java
@@ -8,11 +8,11 @@ package com.project.traceability.common;
  *
  */
 public class PropertyFile {
-	public static final String requirementXMLPath = "E:/Uni/Semi-7/RnD/Product overview documents/RequirementArtefactFile.xml";
-	public static final String umlXMLPath = "E:/Uni/Semi-7/RnD/Product overview documents/UMLArtefactFile.xml";
-	public static final String sourceXMLPath = "E:/Uni/Semi-7/RnD/Product overview documents/SourceCodeArtefactFile.xml";
-	public static final String relationshipXMLPath = "E:/Uni/Semi-7/RnD/Product overview documents/createFile.xml";
-	public static final String graphDbPath = "D:\\Neo4j\\atomdb.graphdb";
-	public static final String generatedGexfFilePath = "C:\\Users\\Thanu\\Documents\\atom-new.gexf";
+	public static final String requirementXMLPath = "F:/Computer/Semester 7/R & D Project/Product overview documents/RequirementArtefactFile.xml";
+	public static final String umlXMLPath = "F:/Computer/Semester 7/R & D Project/Product overview documents/UMLArtefactFile.xml";
+	public static final String sourceXMLPath = "F:/Computer/Semester 7/R & D Project/Product overview documents/SourceCodeArtefactFile.xml";
+	public static final String relationshipXMLPath = "F:/Computer/Semester 7/R & D Project/Product overview documents/createFile.xml";
+	public static final String graphDbPath = "D:\\Neo4j Community\\atomdb.graphdb";
+	public static final String generatedGexfFilePath = "C:\\Users\\Gitanjali\\Documents\\atom-new.gexf";
 }
 

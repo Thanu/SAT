3d56f5b4a3ec279921cdc8d9256f3c749270b80f
Gitanjali <ganjali31@gmail.com>
2014-12-05 16:50:33 +0530

    changed delete code

--- sat/src/main/java/com/project/traceability/GUI/NewProjectWindow.java
+++ sat/src/main/java/com/project/traceability/GUI/NewProjectWindow.java
@@ -30,6 +30,7 @@ public class NewProjectWindow {
 
 	/**
 	 * Launch the application.
+	 * 
 	 * @param args
 	 * @wbp.parser.entryPoint
 	 */
@@ -65,32 +66,32 @@ public class NewProjectWindow {
 		shell = new Shell();
 		shell.setSize(450, 300);
 		shell.setText("SWT Application");
-		
+
 		Label lblProjectName = new Label(shell, SWT.NONE);
 		lblProjectName.setBounds(10, 32, 81, 15);
 		lblProjectName.setText("Project Name :");
-		
+
 		Button btnNewButton = new Button(shell, SWT.SAVE);
 		btnNewButton.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				String projectName = text.getText();
-				projectPath = PropertyFile.filePath + projectName +"/";
+				projectPath = PropertyFile.filePath + projectName + "/";
 				shell.close();
-				HomeGUI.shell.setText("SAT- "+ projectName);
+				HomeGUI.shell.setText("SAT- " + projectName);
 				HomeGUI.tabFolder_1.setVisible(true);
 				HomeGUI.tree.setVisible(true);
-				
+
 				trtmNewTreeitem = new TreeItem(HomeGUI.tree, SWT.NONE);
 				trtmNewTreeitem.setText(projectName);
-				
+
 				File file = new File(projectPath);
 				file.mkdir();
 			}
 		});
 		btnNewButton.setBounds(295, 227, 51, 25);
 		btnNewButton.setText("Create");
-		
+
 		Button btnNewButton_1 = new Button(shell, SWT.NONE);
 		btnNewButton_1.addSelectionListener(new SelectionAdapter() {
 			@Override
@@ -100,111 +101,121 @@ public class NewProjectWindow {
 		});
 		btnNewButton_1.setBounds(359, 227, 65, 25);
 		btnNewButton_1.setText("Cancel");
-		
+
 		text = new Text(shell, SWT.BORDER);
 		text.setBounds(97, 26, 316, 21);
 
 	}
-	
+
 	public static void addPopUpMenu() {
 		Menu popupMenu = new Menu(HomeGUI.tree);
-	    MenuItem newItem = new MenuItem(popupMenu, SWT.CASCADE);
-	    newItem.setText("New");
-	    
-	    MenuItem refreshItem = new MenuItem(popupMenu, SWT.NONE);
-	    refreshItem.addSelectionListener(new SelectionAdapter() {
+		MenuItem newItem = new MenuItem(popupMenu, SWT.CASCADE);
+		newItem.setText("New");
+
+		MenuItem refreshItem = new MenuItem(popupMenu, SWT.NONE);
+		refreshItem.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				HomeGUI.composite.pack(true);
 			}
 		});
-	    refreshItem.setText("Refresh");
-	    
-	    MenuItem deleteItem = new MenuItem(popupMenu, SWT.NONE);
-	    deleteItem.addSelectionListener(new SelectionAdapter() {
+		refreshItem.setText("Refresh");
+
+		MenuItem deleteItem = new MenuItem(popupMenu, SWT.NONE);
+		deleteItem.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				deleteFiles(projectPath);
 			}
 		});
-	    deleteItem.setText("Delete");
-	    
-	    MenuItem compareItem = new MenuItem(popupMenu, SWT.NONE);
-	    compareItem.addSelectionListener(new SelectionAdapter() {
+		deleteItem.setText("Delete");
+
+		MenuItem compareItem = new MenuItem(popupMenu, SWT.NONE);
+		compareItem.addSelectionListener(new SelectionAdapter() {
 			@Override
-			public void widgetSelected(SelectionEvent e) {
+			public void widgetSelected(SelectionEvent e) {				
 				FileSelectionWindow window = new FileSelectionWindow();
 				String string = "";
 				TreeItem[] selection = HomeGUI.tree.getSelection();
-				for (int i = 0; i < selection.length; i++){
+				for (int i = 0; i < selection.length; i++) {
 					string += selection[i] + " ";
 				}
-				string = string.substring(10, string.length()-2);				
+				string = string.substring(10, string.length() - 2);
 				window.open(string);
 			}
 		});
-	    compareItem.setText("Compare Files");
-	    
-	    MenuItem graphItem = new MenuItem(popupMenu, SWT.NONE);
-	    graphItem.addSelectionListener(new SelectionAdapter() {
+		compareItem.setText("Compare Files");
+
+		MenuItem graphItem = new MenuItem(popupMenu, SWT.NONE);
+		graphItem.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				ReadXML.initApp(projectPath);
 			}
 		});
-	    graphItem.setText("Show Graph");
+		graphItem.setText("Show Graph");
 
+		Menu newMenu = new Menu(popupMenu);
+		newItem.setMenu(newMenu);
 
-	    Menu newMenu = new Menu(popupMenu);
-	    newItem.setMenu(newMenu);
-
-	    MenuItem fileItem = new MenuItem(newMenu, SWT.NONE);
-	    fileItem.addSelectionListener(new SelectionAdapter() {
+		MenuItem fileItem = new MenuItem(newMenu, SWT.NONE);
+		fileItem.addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				File file = new File(projectPath);
-				if(!file.isDirectory()){
-					TreeItem parent = NewProjectWindow.trtmNewTreeitem.getParentItem();
-					NewProjectWindow.trtmNewTreeitem = NewProjectWindow.trtmNewTreeitem.getParentItem();
-					projectPath = PropertyFile.filePath + "/" + parent.getText();
+				if (!file.isDirectory()) {
+					TreeItem parent = NewProjectWindow.trtmNewTreeitem
+							.getParentItem();
+					NewProjectWindow.trtmNewTreeitem = NewProjectWindow.trtmNewTreeitem
+							.getParentItem();
+					projectPath = PropertyFile.filePath + parent.getText();
 				}
 				NewFileWindow newFileWin = new NewFileWindow();
-				newFileWin.open();				
+				newFileWin.open();
 			}
 		});
-	    fileItem.setText("File");
-	    
-	    HomeGUI.tree.setMenu(popupMenu);
-	    
+		fileItem.setText("File");
+
+		HomeGUI.tree.setMenu(popupMenu);
+
 	}
-	
+
 	public void center(Shell shell) {
 
-        Rectangle bds = shell.getDisplay().getBounds();
+		Rectangle bds = shell.getDisplay().getBounds();
 
-        Point p = shell.getSize();
-        shell.setFullScreen(true);
-        int nLeft = (bds.width - p.x) / 2;
-        int nTop = (bds.height - p.y) / 2;
+		Point p = shell.getSize();
+		shell.setFullScreen(true);
+		int nLeft = (bds.width - p.x) / 2;
+		int nTop = (bds.height - p.y) / 2;
 
-        shell.setBounds(nLeft, nTop, p.x, p.y);
-    }
-	
+		shell.setBounds(nLeft, nTop, p.x, p.y);
+	}
+
 	public static void deleteFiles(String projectPath) {
-	
-		 MessageBox messageBox = new MessageBox(HomeGUI.shell, SWT.ICON_QUESTION
-		            | SWT.YES | SWT.NO);
-		 messageBox.setMessage("Do you really want to delete " + projectPath + " ?");
-		 messageBox.setText("Deleting " + projectPath);
-		 int response = messageBox.open();
-		 if (response == SWT.YES) {
+
+		MessageBox messageBox = new MessageBox(HomeGUI.shell, SWT.ICON_QUESTION
+				| SWT.YES | SWT.NO);
+		File filePath = new File(projectPath);
+		if (!filePath.isDirectory()) {
+			TreeItem parent = NewProjectWindow.trtmNewTreeitem
+					.getParentItem();
+			projectPath = PropertyFile.filePath 
+					+ parent.getText() + "/"+  NewProjectWindow.trtmNewTreeitem.getText();
+		}
+
+		messageBox.setMessage("Do you really want to delete " + projectPath
+				+ " ?");
+		messageBox.setText("Deleting " + projectPath);
+		int response = messageBox.open();
+		if (response == SWT.YES) {
 			File file = new File(projectPath);
 			String[] files = file.list();
-			if(files != null)
-			for(String stringFile : files){
-				File deleteFile = new File(stringFile);
-				deleteFile.delete();
-			}
+			if (files != null)
+				for (String stringFile : files) {
+					File deleteFile = new File(stringFile);
+					deleteFile.delete();
+				}
 			file.delete();
 		}
 	}

f64fe1d2533eae5ffab54df31090b8ea905995f6
Kamalabalan <kamalan89@gmail.com>
2014-12-05 16:44:01 +0530

    modified class...

--- sat/src/main/java/com/project/traceability/semanticAnalysis/SynonymWords.java
+++ sat/src/main/java/com/project/traceability/semanticAnalysis/SynonymWords.java
@@ -18,27 +18,27 @@ public class SynonymWords {
 		System.setProperty("wordnet.database.dir", "C:\\Program Files (x86)\\WordNet\\2.1\\dict\\");		
 			String wordForm = term;
 			//  Get the synsets containing the wrod form
-			System.out.println(term);
+//			System.out.println(term);
 			Synset[] synsets = database.getSynsets(wordForm);
 			//  Display the word forms and definitions for synsets retrieved
 			if (synsets.length > 0)
 			{
 				for (int i = 0; i < synsets.length; i++)
 				{
-					System.out.println("");
+//					System.out.println("");
 					wordForms = synsets[i].getWordForms();
 					for (int j = 0; j < wordForms.length; j++)
 					{
-						System.out.print((j > 0 ? ", " : "") +
-								wordForms[j]);
+//						System.out.print((j > 0 ? ", " : "") +
+//								wordForms[j]);
 					}
 					
 				}
 			}
 			else
 			{
-				System.err.println("No synsets exist that contain " +
-						"the word form '" + wordForm + "'");
+//				System.err.println("No synsets exist that contain " +
+//						"the word form '" + wordForm + "'");
 			}
 		
 		return wordForms;
@@ -46,24 +46,33 @@ public class SynonymWords {
 	
 	
 	public static boolean checkSymilarity(String term1,String term2){
+		//if the term cotains sub string "NO" at last then change the sub string to "NUMBER"
+ 		if(term1.substring(term1.length()-2).equalsIgnoreCase("No")){
+ 			simpleWord1 =  term1.replace(term1.substring(term1.length()-2), "Number");
+// 			System.out.println("KAMALABALAN"+simpleWord1); 					
+		}else
+			simpleWord1 = term1;
+ 		if(term2.substring(term2.length()-2).equalsIgnoreCase("No")){
+ 			simpleWord2 = term2.replace(term2.substring(term2.length()-2), "Number");
+// 			System.out.println("KAMALABALAN"+simpleWord2);
+		}else
+			simpleWord2 = term2;
 		
 		
 		//remove get or set word from term to check similarity from term1
 		if(term1.contains("get") || term1.contains("set")){
-			simpleWord1 = term1.substring(3);
-			System.out.println("************************"+simpleWord1);						
-		}else
-			simpleWord1 = term1;
+			simpleWord1 = simpleWord1.substring(3);
+//			System.out.println("************************"+simpleWord1);						
+		}
 		
 		//remove get or set word from term to check similarity from term2
 		if(term2.contains("get") || term2.contains("set")){
-			simpleWord2 = term2.substring(3);
-			System.out.println("************************"+simpleWord2);			
-		}else
-			simpleWord2 = term2;
+			simpleWord2 = simpleWord2.substring(3);
+//			System.out.println("************************"+simpleWord2);			
+		}
 		
 		boolean status = false;
-		System.out.println(term1+" KAMAL "+term2);
+//		System.out.println(simpleWord1+" KAMAL "+simpleWord2);
 		
 		//get similar words from WordNet dictionary
 		String[] similarWordForTerm1 = getSynSetWords(simpleWord1);
@@ -84,7 +93,7 @@ public class SynonymWords {
 		
 		}else if(similarWordForTerm1==null && similarWordForTerm2 != null){
 			for(int i=0;i<similarWordForTerm2.length;i++){
-				if(term1.equalsIgnoreCase(similarWordForTerm2[i])){
+				if(simpleWord1.equalsIgnoreCase(similarWordForTerm2[i])){
 					status = true;
 					break;
 				}
@@ -92,27 +101,39 @@ public class SynonymWords {
 			
 		}else if(similarWordForTerm2 == null && similarWordForTerm1 !=null){
 			for(int i=0;i<similarWordForTerm1.length;i++){
-				if(term2.equalsIgnoreCase(similarWordForTerm1[i])){
+				if(simpleWord2.equalsIgnoreCase(similarWordForTerm1[i])){
 					status = true;
 					break;
 				}
 			}
 			
+		}else{
+			status = false;
 		}
 		
+		
+		if(term1.equalsIgnoreCase(term2) || LevenshteinDistance.similarity(term1, term2)>.85){
+			return true;
+		}else
+			return false;
+		
+		
+//		System.out.println(status);
 		
 		//check only 1st letter changed remaining unchanged 
-		if(isFirstletterChanged(term1,term2) ){
-			return false;
-		}
-		//check similarity get the edit distance & if >.85 then it will be ok
-		else if(status || 
-				term1.equalsIgnoreCase(term2) ||
-						LevenshteinDistance.similarity(term1, term2)>.85){
-			return true;
-		}
-		else
-			return false;
+//		if(isFirstletterChanged(term1,term2) ){
+//			return false;
+//		}
+//		//check similarity get the edit distance & if >.85 then it will be ok
+//		else if( 
+//				term1.equalsIgnoreCase(term2) ||
+//						LevenshteinDistance.similarity(term1, term2)>.85){
+//			
+//			System.out.println(term1.equalsIgnoreCase(term2)+" : "+LevenshteinDistance.similarity(term1, term2)+" : "+ term1+"**************"+term2+"TRUE");
+//			return true;
+//		}
+//		else
+//			return false;
 		
 		
 	}

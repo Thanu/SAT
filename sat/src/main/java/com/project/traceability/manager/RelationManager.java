7c35ab6078a6f1c51c768cad21c86e1a5b12b258
Gitanjali <ganjali31@gmail.com>
2014-12-15 10:27:48 +0530

    made variables static

--- sat/src/main/java/com/project/traceability/manager/RelationManager.java
+++ sat/src/main/java/com/project/traceability/manager/RelationManager.java
@@ -1,5 +1,6 @@
 package com.project.traceability.manager;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.parsers.DocumentBuilder;
@@ -20,6 +21,9 @@ import com.project.traceability.common.PropertyFile;
 
 public class RelationManager {
 
+	public static Document document;
+	public static Element rootElement;
+	
 	public static void createXML(List<String> relationNodes) {
 		try {
 
@@ -29,33 +33,32 @@ public class RelationManager {
 					.newDocumentBuilder();
 
 			// define root elements
-			Document document = documentBuilder.newDocument();
-			Element rootElement = document.createElement("Relations");
+			document = documentBuilder.newDocument();
+			rootElement = document.createElement("Relations");
 			document.appendChild(rootElement);
 
+			for(int i = 0, j = 1; i < relationNodes.size(); i++,j++){
 			
+			// define school elements
+			Element school = document.createElement("Relation");
+			rootElement.appendChild(school);
+
+			// add attributes to school
+			Attr attribute = document.createAttribute("id");
+			attribute.setValue("" + j + "");
+			school.setAttributeNode(attribute);
+			//System.out.println(relationNodes.get(i));
+			Element firstname = document.createElement("SourceNode");
+			firstname.appendChild(document.createTextNode(relationNodes.get(i)));
+			school.appendChild(firstname);
+
+			// lastname elements
+			Element lastname = document.createElement("TargetNode");
+			lastname.appendChild(document.createTextNode(relationNodes.get(++i)));
+			school.appendChild(lastname);
+		}
 			
 			//System.out.println(relationNodes.size());
-			for(int i = 0, j = 1; i < relationNodes.size(); i++,j++){
-				
-				// define school elements
-				Element school = document.createElement("Relation");
-				rootElement.appendChild(school);
-
-				// add attributes to school
-				Attr attribute = document.createAttribute("id");
-				attribute.setValue("" + j + "");
-				school.setAttributeNode(attribute);
-				//System.out.println(relationNodes.get(i));
-				Element firstname = document.createElement("SourceNode");
-				firstname.appendChild(document.createTextNode(relationNodes.get(i)));
-				school.appendChild(firstname);
-
-				// lastname elements
-				Element lastname = document.createElement("TargetNode");
-				lastname.appendChild(document.createTextNode(relationNodes.get(++i)));
-				school.appendChild(lastname);
-			}
 			
 
 			// creating and writing to xml file
@@ -77,5 +80,5 @@ public class RelationManager {
 			tfe.printStackTrace();
 		}
 	}
-
+	
 }

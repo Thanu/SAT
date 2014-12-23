319f316f6ec3f939434299dc961aa521bf699265
Gitanjali <ganjali31@gmail.com>
2014-12-23 18:32:12 +0530

    uncommented

--- sat/src/main/java/com/project/traceability/manager/EditManager.java
+++ sat/src/main/java/com/project/traceability/manager/EditManager.java
@@ -3,6 +3,8 @@
  */
 package com.project.traceability.manager;
 
+import java.util.ArrayList;
+
 import com.project.traceability.model.ArtefactElement;
 
 
@@ -19,9 +21,9 @@ public class EditManager {
 	public static void addLink(ArtefactElement className, ArtefactElement subElementName){
 		System.out.println(className + " " +subElementName);
 		//ArrayList<String> relations = new ArrayList<String>();
-		//ReadXML.relationNodes.add(className);
-		//ReadXML.relationNodes.add(subElementName);
-		//RelationManager.createXML(ReadXML.relationNodes);
+		ReadXML.relationNodes.add(className.getArtefactElementId());
+		ReadXML.relationNodes.add(subElementName.getArtefactElementId());
+		RelationManager.createXML(ReadXML.relationNodes);
 	}
 
 }

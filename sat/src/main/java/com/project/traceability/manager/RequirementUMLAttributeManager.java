e9a073ddc856fb7963695ab794f99ded56351fc9
Kamalabalan <kamalan89@gmail.com>
2014-12-30 07:28:37 +0530

    class modified...

--- sat/src/main/java/com/project/traceability/manager/RequirementUMLAttributeManager.java
+++ sat/src/main/java/com/project/traceability/manager/RequirementUMLAttributeManager.java
@@ -11,95 +11,100 @@ import com.project.traceability.model.ArtefactElement;
 import com.project.traceability.model.ArtefactSubElement;
 import com.project.traceability.model.AttributeModel;
 import com.project.traceability.semanticAnalysis.SynonymWords;
+import com.project.traceability.semanticAnalysis.WordsMap;
 import com.project.traceability.utils.Constants.ArtefactSubElementType;
 
 /**
  * 19 Nov 2014
- * 
+ *
  * @author K.Kamalan
  *
  */
 public class RequirementUMLAttributeManager {
 
-	static List<String> relationNodes = new ArrayList<String>();
-	static Map<ArtefactElement, List<? extends ArtefactSubElement>> reqSubArtefacts = RequirementsManger
-			.manageArtefactSubElements(ArtefactSubElementType.ATTRIBUTE); // get
-																			// read
-																			// source
-																			// code
-																			// attribute
-																			// artifacts
-	static Map<ArtefactElement, List<ArtefactSubElement>> UMLSubArtefacts = UMLArtefactManager
-			.manageArtefactSubElements(ArtefactSubElementType.ATTRIBUTE);
-	static String projectPath;
+    static List<String> relationNodes = new ArrayList<String>();
+    static Map<ArtefactElement, List<? extends ArtefactSubElement>> reqSubArtefacts = RequirementsManger
+            .manageArtefactSubElements(ArtefactSubElementType.ATTRIBUTE); // get
+    // read
+    // source
+    // code
+    // attribute
+    // artifacts
+    static Map<ArtefactElement, List<ArtefactSubElement>> UMLSubArtefacts = UMLArtefactManager
+            .manageArtefactSubElements(ArtefactSubElementType.ATTRIBUTE);
+    static String projectPath;
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public static List<String> mapAttributes(String projectPath) {
-		Map<ArtefactElement, List<? extends ArtefactSubElement>> reqAttributeArtefactMap = reqSubArtefacts;
-		Map<ArtefactElement, List<ArtefactSubElement>> UMLattributeArtefactMap = UMLSubArtefacts;
-		Iterator<Entry<ArtefactElement, List<ArtefactSubElement>>> UMLIterator = UMLattributeArtefactMap
-				.entrySet().iterator();
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public static List<String> mapAttributes(String projectPath) {
+        Map<ArtefactElement, List<? extends ArtefactSubElement>> reqAttributeArtefactMap = reqSubArtefacts;
+        Map<ArtefactElement, List<ArtefactSubElement>> UMLattributeArtefactMap = UMLSubArtefacts;
+        Iterator<Entry<ArtefactElement, List<ArtefactSubElement>>> UMLIterator = UMLattributeArtefactMap
+                .entrySet().iterator();
 
-		while (UMLIterator.hasNext()) {
-			Map.Entry UMLPairs = UMLIterator.next();
-			ArtefactElement UMLArtefactElement = (ArtefactElement) UMLPairs
-					.getKey();
-			List<AttributeModel> UMLAttributeElements = (List<AttributeModel>) UMLPairs
-					.getValue();
-			Iterator<Entry<ArtefactElement, List<? extends ArtefactSubElement>>> reqIterator = reqAttributeArtefactMap
-					.entrySet().iterator();
-			while (reqIterator.hasNext()) {
-				Map.Entry reqPairs = reqIterator.next();
-				ArtefactElement reqArtefactElement = (ArtefactElement) reqPairs
-						.getKey();
-				List<AttributeModel> reqAttributeElements = (List<AttributeModel>) reqPairs
-						.getValue();
-				if(SynonymWords.checkSymilarity(reqArtefactElement.getName(), UMLArtefactElement.getName(),
-						reqArtefactElement.getType())){
+        while (UMLIterator.hasNext()) {
+            Map.Entry UMLPairs = UMLIterator.next();
+            ArtefactElement UMLArtefactElement = (ArtefactElement) UMLPairs
+                    .getKey();
+            List<AttributeModel> UMLAttributeElements = (List<AttributeModel>) UMLPairs
+                    .getValue();
+            Iterator<Entry<ArtefactElement, List<? extends ArtefactSubElement>>> reqIterator = reqAttributeArtefactMap
+                    .entrySet().iterator();
+            while (reqIterator.hasNext()) {
+                Map.Entry reqPairs = reqIterator.next();
+                ArtefactElement reqArtefactElement = (ArtefactElement) reqPairs
+                        .getKey();
+                List<AttributeModel> reqAttributeElements = (List<AttributeModel>) reqPairs
+                        .getValue();
+                WordsMap w1 = new WordsMap();
+                w1 = SynonymWords.checkSymilarity(reqArtefactElement.getName(), UMLArtefactElement.getName(),
+                        reqArtefactElement.getType()); 
+                                if (w1.isIsMatched()) {
 //				if (reqArtefactElement.getName().equalsIgnoreCase(
 //						UMLArtefactElement.getName())
 //						|| LevenshteinDistance.similarity(
 //								reqArtefactElement.getName(),
 //								UMLArtefactElement.getName()) > .6) {
-					for (int i = 0; i < UMLAttributeElements.size(); i++) {
-						AttributeModel UMLAttribute = UMLAttributeElements
-								.get(i);
-						for (int j = 0; j < reqAttributeElements.size(); j++) {
-							AttributeModel reqAttribute = reqAttributeElements
-									.get(j);
-							
-							if(SynonymWords.checkSymilarity(UMLAttribute.getName(), reqAttribute.getName(),
-									UMLAttribute.getType())){
+                    for (int i = 0; i < UMLAttributeElements.size(); i++) {
+                        AttributeModel UMLAttribute = UMLAttributeElements
+                                .get(i);
+                        for (int j = 0; j < reqAttributeElements.size(); j++) {
+                            AttributeModel reqAttribute = reqAttributeElements
+                                    .get(j);
+                            WordsMap w2 = new WordsMap();
+                            w2 = SynonymWords.checkSymilarity(UMLAttribute.getName(), reqAttribute.getName(),
+                                    UMLAttribute.getType());
+                            if (w2.isIsMatched()) {
 
 //							if (UMLAttribute.getName().equalsIgnoreCase(
 //									reqAttribute.getName())
 //									|| LevenshteinDistance.similarity(
 //											UMLAttribute.getName(),
 //											reqAttribute.getName()) > .6) {
-
-								// System.out.println(UMLAttribute.getSubElementId()+"@@@@@@@@@@@@"+reqAttribute.getSubElementId());
-								if (SynonymWords.checkSymilarity(
-										UMLAttribute.getName(),
-										reqAttribute.getName(),UMLAttribute.getType())) {
+                                // System.out.println(UMLAttribute.getSubElementId()+"@@@@@@@@@@@@"+reqAttribute.getSubElementId());
+                                WordsMap w3 = new WordsMap();
+                                w3 = SynonymWords.checkSymilarity(
+                                        UMLAttribute.getName(),
+                                        reqAttribute.getName(), UMLAttribute.getType());
+                                if (w3.isIsMatched()) {
 									// if
-									// (UMLAttribute.getName().equalsIgnoreCase(reqAttribute.getName())||LevenshteinDistance.similarity(UMLAttribute.getName(),
-									// reqAttribute.getName())>.6) {
+                                    // (UMLAttribute.getName().equalsIgnoreCase(reqAttribute.getName())||LevenshteinDistance.similarity(UMLAttribute.getName(),
+                                    // reqAttribute.getName())>.6) {
 
-									relationNodes.add(reqAttribute
-											.getSubElementId().substring(
-													reqAttribute
-															.getSubElementId()
-															.length() - 3));
-									relationNodes.add(UMLAttribute
-											.getSubElementId());
-									UMLAttributeElements.remove(UMLAttribute);
-									reqAttributeElements.remove(reqAttribute);
-									i--;
-									j--;
-									break;
-								}
-							}
-						}
+                                    relationNodes.add(reqAttribute
+                                            .getSubElementId().substring(
+                                                    reqAttribute
+                                                    .getSubElementId()
+                                                    .length() - 3));
+                                    relationNodes.add(UMLAttribute
+                                            .getSubElementId());
+                                    UMLAttributeElements.remove(UMLAttribute);
+                                    reqAttributeElements.remove(reqAttribute);
+                                    i--;
+                                    j--;
+                                    break;
+                                }
+                            }
+                        }
 
 //						if (UMLAttributeElements.size() > 0
 //								|| reqAttributeElements.size() > 0) {
@@ -124,7 +129,7 @@ public class RequirementUMLAttributeManager {
 //							}
 //
 //					}
-					}
+                    }
 //					if(UMLAttributeElements.size() > 0 || reqAttributeElements.size() > 0) {
 //						System.out.println("There are some conflicts among attributes in "+ reqArtefactElement.getName() 
 //									+ " class.");
@@ -143,14 +148,13 @@ public class RequirementUMLAttributeManager {
 //
 //						}
 //					}
-				}
-				
-			}
-			UMLIterator.remove();
+                }
 
-		}
-		return relationNodes;
+            }
+            UMLIterator.remove();
 
+        }
+        return relationNodes;
 
-	}
+    }
 }
--- sat/src/main/java/com/project/traceability/manager/RequirementUMLMethodManager.java
+++ sat/src/main/java/com/project/traceability/manager/RequirementUMLMethodManager.java
@@ -11,51 +11,56 @@ import com.project.traceability.model.ArtefactElement;
 import com.project.traceability.model.ArtefactSubElement;
 import com.project.traceability.model.MethodModel;
 import com.project.traceability.semanticAnalysis.SynonymWords;
+import com.project.traceability.semanticAnalysis.WordsMap;
 import com.project.traceability.utils.Constants.ArtefactSubElementType;
 
 /**
  * 19 Nov 2014
- * 
+ *
  * @author K.Kamalan
- * 
+ *
  */
 public class RequirementUMLMethodManager {
-	static List<String> relationNodes = new ArrayList<String>();
 
-	@SuppressWarnings({ "unchecked", "rawtypes" })
-	public static List<String> mapAttributes(String projecpath) {
-		Map<ArtefactElement, List<? extends ArtefactSubElement>> reqAttributeArtefactMap = RequirementsManger
-				.manageArtefactSubElements(ArtefactSubElementType.METHOD);
-		Map<ArtefactElement, List<ArtefactSubElement>> UMLattributeArtefactMap = UMLArtefactManager
-				.manageArtefactSubElements(ArtefactSubElementType.METHOD);
+    static List<String> relationNodes = new ArrayList<String>();
 
-		Iterator<Entry<ArtefactElement, List<ArtefactSubElement>>> UMLIterator = UMLattributeArtefactMap
-				.entrySet().iterator();
-		while (UMLIterator.hasNext()) {
-			Map.Entry UMLPairs = UMLIterator.next();
-			ArtefactElement UMLArtefactElement = (ArtefactElement) UMLPairs
-					.getKey();
-			List<ArtefactSubElement> UMLAttributeElements = (List<ArtefactSubElement>) UMLPairs
-					.getValue();
-			Iterator<Entry<ArtefactElement, List<? extends ArtefactSubElement>>> reqIterator = reqAttributeArtefactMap
-					.entrySet().iterator();
-			while (reqIterator.hasNext()) {
-				Map.Entry reqPairs = reqIterator.next();
-				ArtefactElement reqArtefactElement = (ArtefactElement) reqPairs
-						.getKey();
-				List<ArtefactSubElement> reqAttributeElements = (List<ArtefactSubElement>) reqPairs
-						.getValue();
-				if(SynonymWords.checkSymilarity(reqArtefactElement.getName(), UMLArtefactElement.getName(),reqArtefactElement.getType())){
+    @SuppressWarnings({"unchecked", "rawtypes"})
+    public static List<String> mapAttributes(String projecpath) {
+        Map<ArtefactElement, List<? extends ArtefactSubElement>> reqAttributeArtefactMap = RequirementsManger
+                .manageArtefactSubElements(ArtefactSubElementType.METHOD);
+        Map<ArtefactElement, List<ArtefactSubElement>> UMLattributeArtefactMap = UMLArtefactManager
+                .manageArtefactSubElements(ArtefactSubElementType.METHOD);
+
+        Iterator<Entry<ArtefactElement, List<ArtefactSubElement>>> UMLIterator = UMLattributeArtefactMap
+                .entrySet().iterator();
+        while (UMLIterator.hasNext()) {
+            Map.Entry UMLPairs = UMLIterator.next();
+            ArtefactElement UMLArtefactElement = (ArtefactElement) UMLPairs
+                    .getKey();
+            List<ArtefactSubElement> UMLAttributeElements = (List<ArtefactSubElement>) UMLPairs
+                    .getValue();
+            Iterator<Entry<ArtefactElement, List<? extends ArtefactSubElement>>> reqIterator = reqAttributeArtefactMap
+                    .entrySet().iterator();
+            while (reqIterator.hasNext()) {
+                Map.Entry reqPairs = reqIterator.next();
+                ArtefactElement reqArtefactElement = (ArtefactElement) reqPairs
+                        .getKey();
+                List<ArtefactSubElement> reqAttributeElements = (List<ArtefactSubElement>) reqPairs
+                        .getValue();
+                WordsMap w1 = new WordsMap();
+                w1 = SynonymWords.checkSymilarity(reqArtefactElement.getName(), UMLArtefactElement.getName(), reqArtefactElement.getType());
+                                if (w1.isIsMatched()) {
 //				if (reqArtefactElement.getName().equalsIgnoreCase(
 //						UMLArtefactElement.getName())
 //						|| LevenshteinDistance.similarity(
 //								reqArtefactElement.getName(),
 //								UMLArtefactElement.getName()) > .6) {
-					for (int i = 0; i < UMLAttributeElements.size(); i++) {
-						for (int j = 0; j < reqAttributeElements.size(); j++) {
-
-							if(SynonymWords.checkSymilarity(UMLAttributeElements.get(i).getName(), reqAttributeElements.get(j).getName(),
-									UMLAttributeElements.get(i).getType())){
+                    for (int i = 0; i < UMLAttributeElements.size(); i++) {
+                        for (int j = 0; j < reqAttributeElements.size(); j++) {
+                            WordsMap w2 = new WordsMap();
+                            w2 = SynonymWords.checkSymilarity(UMLAttributeElements.get(i).getName(), reqAttributeElements.get(j).getName(),
+                                    UMLAttributeElements.get(i).getType());
+                            if (w2.isIsMatched()) {
 //							if (UMLAttributeElements
 //									.get(i)
 //									.getName()
@@ -68,33 +73,35 @@ public class RequirementUMLMethodManager {
 //											reqAttributeElements.get(j)
 //													.getName()) > .6) {
 
-								// System.out.println(UMLAttributeElements.get(i).getSubElementId()+"***********"+reqAttributeElements.get(j).getSubElementId());
-								if (SynonymWords.checkSymilarity(
-										UMLAttributeElements.get(i).getName(),
-										reqAttributeElements.get(j).getName(),UMLAttributeElements.get(i).getType())) {
+                                // System.out.println(UMLAttributeElements.get(i).getSubElementId()+"***********"+reqAttributeElements.get(j).getSubElementId());
+                                WordsMap w3 = new WordsMap();
+                                w3 = SynonymWords.checkSymilarity(
+                                        UMLAttributeElements.get(i).getName(),
+                                        reqAttributeElements.get(j).getName(), UMLAttributeElements.get(i).getType());
+                                if (w3.isIsMatched()) {
 									// if(UMLAttributeElements.get(i).getName().equalsIgnoreCase
-									// (reqAttributeElements.get(j).getName())||LevenshteinDistance.similarity(UMLAttributeElements.get(i).getName(),
-									// reqAttributeElements.get(j).getName())>.6){
+                                    // (reqAttributeElements.get(j).getName())||LevenshteinDistance.similarity(UMLAttributeElements.get(i).getName(),
+                                    // reqAttributeElements.get(j).getName())>.6){
 
-									relationNodes.add(reqAttributeElements
-											.get(j)
-											.getSubElementId()
-											.substring(
-													reqAttributeElements.get(j)
-															.getSubElementId()
-															.length() - 3));
-									relationNodes.add(UMLAttributeElements.get(
-											i).getSubElementId());
-									UMLAttributeElements.remove(i); // remove
-																	// mapped
-																	// objects
-									i--;
-									reqAttributeElements.remove(j);
-									j--;
-									break;
-								}
-							}
-						}
+                                    relationNodes.add(reqAttributeElements
+                                            .get(j)
+                                            .getSubElementId()
+                                            .substring(
+                                                    reqAttributeElements.get(j)
+                                                    .getSubElementId()
+                                                    .length() - 3));
+                                    relationNodes.add(UMLAttributeElements.get(
+                                            i).getSubElementId());
+                                    UMLAttributeElements.remove(i); // remove
+                                    // mapped
+                                    // objects
+                                    i--;
+                                    reqAttributeElements.remove(j);
+                                    j--;
+                                    break;
+                                }
+                            }
+                        }
 
 //						if (UMLAttributeElements.size() > 0
 //								|| reqAttributeElements.size() > 0) {
@@ -146,16 +153,16 @@ public class RequirementUMLMethodManager {
 //
 //							}
 //						}
-					}
-					
-				}
+                    }
 
-			}
-			UMLIterator.remove();
-		}
+                }
 
-		return relationNodes;
+            }
+            UMLIterator.remove();
+        }
 
-	}
+        return relationNodes;
+
+    }
 
 }

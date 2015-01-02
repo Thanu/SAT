841fc9769ac9e4e1f124fad5270bb6679f6be9b3
Thanuja Uruthira <uthanuja@yahoo.com>
2015-01-02 10:21:11 +0530

    image path added.

--- sat/src/main/java/com/project/traceability/common/PropertyFile.java
+++ sat/src/main/java/com/project/traceability/common/PropertyFile.java
@@ -14,10 +14,10 @@ public class PropertyFile {
 	public static final String umlXMLPath = "E:/ATOM/UMLArtefactFile.xml";
 	public static final String sourceXMLPath = "E:/ATOM/SourceCodeArtefactFile.xml";
 	public static final String relationshipXMLPath = "E:/ATOM/createFile.xml";
-<<<<<<< HEAD
 	public static final String wordNetDbDirectory = "E:/Programs/WordNet/dict";
 	public static final String filePath = "E:/SATWork/";
 	public static final String xmlFilePath = "E:/ATOM";
+        public static final String imagePath = "E:/SAT/sat/img";		//img folder in project
         public static String projectName  = null; 
         public static String graphDbPath = null;//NewProjectWindow.projectPath+"/"+projectName+".graphdb";
 	public static String generatedGexfFilePath = null;//NewProjectWindow.projectPath+"/"+projectName+".gexf";
@@ -47,15 +47,5 @@ public class PropertyFile {
         PropertyFile.generatedGexfFilePath = generatedGexfFilePath;
     }
     
-    
-        
-=======
-	public static final String graphDbPath = "E:/ATOM/Neo4j/atomdb.graphdb";
-	public static final String generatedGexfFilePath = "E:/ATOM/atom.gexf";
-	public static final String wordNetDbDirectory = "G:/WordNet/dict";
-	public static final String filePath = "E:/SATWork/";
-	public static final String xmlFilePath = "E:/ATOM";
-	public static final String imagePath = "E:/SAT/sat/img";		//img folder in project
->>>>>>> 2798a7a7b0cf04c25ed655a6f9dfb101445dbc67
 }
 

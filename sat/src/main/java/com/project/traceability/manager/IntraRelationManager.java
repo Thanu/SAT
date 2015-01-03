c8b3fd5cf51785f83cf423d72c4e42f8bb0297f2
Kamalabalan <kamalan89@gmail.com>
2015-01-03 18:02:37 +0530

    new class added for create intra relation...

--- sat/src/main/java/com/project/traceability/manager/IntraRelationManager.java
+++ sat/src/main/java/com/project/traceability/manager/IntraRelationManager.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.project.traceability.manager;
 
 import com.project.traceability.model.ArtefactElement;
@@ -18,16 +17,17 @@ import java.util.Map;
  * @author K.Kamalan
  */
 public class IntraRelationManager {
+
     static List<String> relationNodes = new ArrayList<String>();
-    
+
     static String projectPath;
-    
+
     public static void getReqIntraRelation(String projectPath) {
         IntraRelationManager.projectPath = projectPath;
         RequirementsManger.readXML(projectPath);
         Map<String, ArtefactElement> reqMap = RequirementsManger.requirementArtefactElements;
         Iterator<Map.Entry<String, ArtefactElement>> requirementIterator = reqMap
-				.entrySet().iterator();
+                .entrySet().iterator();
         while (requirementIterator.hasNext()) {
             Map.Entry pairs = requirementIterator.next();
             ArtefactElement reqArtefactElement = (ArtefactElement) pairs
@@ -35,32 +35,30 @@ public class IntraRelationManager {
             List<ArtefactSubElement> reqAttributeElements = reqArtefactElement
                     .getArtefactSubElements();
             if (reqArtefactElement.getType().equalsIgnoreCase("Class")) {
-                System.out.println(reqArtefactElement.getName()+"%%%%%%%%%%%%%%%%");
+                System.out.println(reqArtefactElement.getName() + "%%%%%%%%%%%%%%%%");
                 compareSubElements(reqArtefactElement);
             }
         }
-        
-        
-        
+
 //        return relationNodes;
     }
-    
+
     public static void compareSubElements(ArtefactElement reqArtefactElement) {
         List<ArtefactSubElement> reqAttributeElements = reqArtefactElement
                 .getArtefactSubElements();
         for (int i = 0; i < reqAttributeElements.size() && reqAttributeElements.get(i).getType().equalsIgnoreCase("Field"); i++) {
-            System.out.println("KAMALA"+reqAttributeElements.get(i).getName());
+            System.out.println("KAMALA" + reqAttributeElements.get(i).getName());
             int count = 0;
             for (int j = 0; j < reqAttributeElements.size(); j++) {
-                if(reqAttributeElements.get(j).getType().equalsIgnoreCase("Method")){
-                    if(reqAttributeElements.get(i).getName().equalsIgnoreCase(reqAttributeElements.get(j).getName().substring(3))){
-                        System.out.println(reqAttributeElements.get(i).getName()+"KAMALAN"+reqAttributeElements.get(j).getName());
+                if (reqAttributeElements.get(j).getType().equalsIgnoreCase("Method")) {
+                    if (reqAttributeElements.get(i).getName().equalsIgnoreCase(reqAttributeElements.get(j).getName().substring(3))) {
+                        System.out.println(reqAttributeElements.get(i).getName() + "KAMALAN" + reqAttributeElements.get(j).getName());
                         count++;
-                        if(count==2){
+                        if (count == 2) {
                             break;
-                        }                     
-                }
-                    
+                        }
+                    }
+
                 }
 
             }

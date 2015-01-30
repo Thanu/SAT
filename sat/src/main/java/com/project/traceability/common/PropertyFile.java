b7bdbf98cd851dca3eb8384bb36fa46bbbdf6b0c
Gitanjali <ganjali31@gmail.com>
2015-01-30 18:54:55 +0530

    added some testing variables

--- sat/src/main/java/com/project/traceability/common/PropertyFile.java
+++ sat/src/main/java/com/project/traceability/common/PropertyFile.java
@@ -3,67 +3,69 @@
  */
 package com.project.traceability.common;
 
-import com.project.traceability.GUI.NewProjectWindow;
-
-/**13 Nov 2014
+/**
+ * 13 Nov 2014
+ * 
  * @author K.Kamalan
- *
+ * 
  */
 public class PropertyFile {
 	public static final String requirementXMLPath = "E:/ATOM/RequirementArtefactFile.xml";
 	public static final String umlXMLPath = "E:/ATOM/UMLArtefactFile.xml";
 	public static final String sourceXMLPath = "E:/ATOM/SourceCodeArtefactFile.xml";
-	
-	public static final String wordNetDbDirectory = "E:/Programs/WordNet/dict";
-	public static final String filePath = "E:/SATWork/";
-	public static final String xmlFilePath = "E:/ATOM";
-        public static final String imagePath = "E:/SAT/sat/img";		//img folder in project
-        public static String projectName  = null; 
-        public static String graphDbPath = null;//NewProjectWindow.projectPath+"/"+projectName+".graphdb";
-	public static String generatedGexfFilePath = null;//NewProjectWindow.projectPath+"/"+projectName+".gexf";
-        public static String relationshipXMLPath = null;//"E:/ATOM/createFile.xml";
-        public static String graphType = null;
-	
 
-    public static String getProjectName() {
-        return projectName;
-    }
+	public static final String wordNetDbDirectory = "G:/WordNet/dict";
+	public static String filePath = "E:/SATWork/";
+	public static String xmlFilePath = "E:/ATOM";
+	public static final String imagePath = "E:/SAT/sat/img"; // img folder in
+																// project
+	public static String projectName = null;
+	public static String graphDbPath = null;// NewProjectWindow.projectPath+"/"+projectName+".graphdb";
+	public static String generatedGexfFilePath = null;// NewProjectWindow.projectPath+"/"+projectName+".gexf";
+	public static String relationshipXMLPath = null;// "E:/ATOM/createFile.xml";
+	public static String graphType = null;
 
-    public static void setProjectName(String projectName) {
-        PropertyFile.projectName = projectName;
-    }
+	public static final String testFilePath = "E:/SATWork/test/";
+	public static final String testXmlFilePath = "E:/ATOM/test/";
 
-    public static String getGraphDbPath() {
-        return graphDbPath;
-    }
+	public static String getProjectName() {
+		return projectName;
+	}
 
-    public static void setGraphDbPath(String graphDbPath) {
-        PropertyFile.graphDbPath = graphDbPath;
-    }
+	public static void setProjectName(String projectName) {
+		PropertyFile.projectName = projectName;
+	}
 
-    public static String getGeneratedGexfFilePath() {
-        return generatedGexfFilePath;
-    }
+	public static String getGraphDbPath() {
+		return graphDbPath;
+	}
 
-    public static void setGeneratedGexfFilePath(String generatedGexfFilePath) {
-        PropertyFile.generatedGexfFilePath = generatedGexfFilePath;
-    }
+	public static void setGraphDbPath(String graphDbPath) {
+		PropertyFile.graphDbPath = graphDbPath;
+	}
 
-    public static String getRelationshipXMLPath() {
-        return relationshipXMLPath;
-    }
+	public static String getGeneratedGexfFilePath() {
+		return generatedGexfFilePath;
+	}
 
-    public static void setRelationshipXMLPath(String relationshipXMLPath) {
-        PropertyFile.relationshipXMLPath = relationshipXMLPath;
-    }
+	public static void setGeneratedGexfFilePath(String generatedGexfFilePath) {
+		PropertyFile.generatedGexfFilePath = generatedGexfFilePath;
+	}
 
-    public static String getGraphType() {
-        return graphType;
-    }
+	public static String getRelationshipXMLPath() {
+		return relationshipXMLPath;
+	}
 
-    public static void setGraphType(String graphType) {
-        PropertyFile.graphType = graphType;
-    }
-        
+	public static void setRelationshipXMLPath(String relationshipXMLPath) {
+		PropertyFile.relationshipXMLPath = relationshipXMLPath;
+	}
+
+	public static String getGraphType() {
+		return graphType;
+	}
+
+	public static void setGraphType(String graphType) {
+		PropertyFile.graphType = graphType;
+	}
+
 }
-
